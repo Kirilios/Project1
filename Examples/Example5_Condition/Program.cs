9601f2472f43b30e36e587f91349762727de5265
Kirilios <kirilios17@mail.ru>
2023-07-30 16:37:01 +0300

    p

---
+++ Examples/Example5_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter Name")
+string username = Console.ReadLine();
+
+if(username == "Kirill")
+{
+    Console.WriteLine("Yes, that's my BOY");
+}
+else
+[
+    Console.Write("Hello,");
+    Console.WriteLine(username);
+]
